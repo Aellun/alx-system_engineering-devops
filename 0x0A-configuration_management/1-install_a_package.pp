dc0c09aaf6a1002c161475431624e5ea6027855e
Aellun <okello.k.otieono@gmail.com>
2023-12-25 00:48:34 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,23 @@
-#!/usr/bin/pup
-# install flask version 2.1.0
-package {'flask':
+# Define a package resource for Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Define a package resource for Flask with version 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3.8'],
+}
+
+# Define a package resource for Werkzeug with version 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
+# Notify the user when the packages are installed
+notify { 'Packages installed':
+  require => Package['Werkzeug'],
 }
