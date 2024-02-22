fda91b85f5f7b62a7f2b9cd1d55a1bab951c305e
Aellun <okeelo.k.otieno@gmail.com>
2024-02-22 14:43:53 +0300

    lint correction

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,17 @@
-# This script enables the user Holberton to logina and change files
+# This script enables the user Holberton to log in and change files
 
 # Increase hard file limit for user holberton
 file_line { 'increase-hard-file-limit-for-holberton-user':
-  path    => '/etc/security/limits.conf',
-  line    => 'holberton hard nofile 50000',
-  match   => 'holberton hard nofile',
-  ensure  => present,
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton hard nofile 50000',
+  match  => '^holberton hard nofile',
 }
 
 # Increase soft file limit for user holberton
 file_line { 'increase-soft-file-limit-for-holberton-user':
-  path    => '/etc/security/limits.conf',
-  line    => 'holberton soft nofile 50000',
-  match   => 'holberton soft nofile',
-  ensure  => present,
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton soft nofile 50000',
+  match  => '^holberton soft nofile',
 }
