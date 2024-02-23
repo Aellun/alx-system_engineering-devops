01d3bed9d5f83217374db7878bf28e589fdc6f1a
Aellun <okeelo.k.otieno@gmail.com>
2024-02-23 05:36:57 +0300

    updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # Increase hard file limit for user holberton
 exec { 'increase_hard_file_limit_for_holberton':
-  command => 'sed -i "/holberton hard/s/15/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
 
 # Increase soft file limit for user holberton
 exec { 'increase_soft_file_limit_for_holberton':
-  command => 'sed -i "/holberton soft/s/10/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
