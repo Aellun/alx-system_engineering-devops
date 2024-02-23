b298dc05c64fd978882dc4c7304355bb7794f5d7
Aellun <okeelo.k.otieno@gmail.com>
2024-02-23 05:30:07 +0300

    updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,22 +1,13 @@
 # This script increase the amount of request the nginx server can handle
 
-# Manage nginx configuration file
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => template('your_module/nginx_config.erb'),
-  notify  => Exec['nginx-restart'],
+# Increase ULIMIT for the default file
+exec { 'adjust_ulimit_for_nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
 
-# Exec resource to restart Nginx
-exec { 'nginx-restart':
-  command     => 'systemctl restart nginx',
-  refreshonly => true,
-  path        => ['/bin', '/usr/bin'],
-}
-
-# Modify max open files limit using sed
-exec { 'modify-max-open-files-limit':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx',
-  path    => '/bin:/usr/bin', # Adjust the path as needed
-  notify  => Exec['nginx-restart'],
+# Restart Nginx
+-> exec { 'restart_nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
