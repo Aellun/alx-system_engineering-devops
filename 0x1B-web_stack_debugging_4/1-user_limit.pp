6465afec163bce3869c17fec5b1ea74499891ffb
Aellun <okeelo.k.otieno@gmail.com>
2024-02-23 05:35:12 +0300

    updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,13 @@
 # This script enables the user Holberton to log in and change files
 
 # Increase hard file limit for user holberton
-file_line { 'increase-hard-file-limit-for-holberton-user':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton hard nofile 50000',
-  match  => '^holberton hard nofile',
+exec { 'increase_hard_file_limit_for_holberton':
+  command => 'sed -i "/holberton hard/s/15/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Increase soft file limit for user holberton
-file_line { 'increase-soft-file-limit-for-holberton-user':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton soft nofile 50000',
-  match  => '^holberton soft nofile',
+exec { 'increase_soft_file_limit_for_holberton':
+  command => 'sed -i "/holberton soft/s/10/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
