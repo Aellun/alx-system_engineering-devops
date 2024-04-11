55570da353e92259702dc33ebf12c506be48d1e0
Aellun <okeelo.k.otieno@gmail.com>
2024-04-11 21:04:12 +0300

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit."""
 
-from requests import get
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
+    """Return the top 10 hot posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyBot/1.0'}
 
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-        return
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    user_agent = {'User-agent': 'Google Chrome Version 24.0.6367.37'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
 
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+        print(f"Top 10 hot posts in r/{subreddit}:")
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print("None")
