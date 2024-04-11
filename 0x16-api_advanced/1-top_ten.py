49f60b56277c43aa9dc39894fdb4dbc232e7cb55
Aellun <okeelo.k.otieno@gmail.com>
2024-04-11 20:52:48 +0300

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 from requests import get
 """function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit."""
@@ -15,18 +14,18 @@ def top_ten(subreddit):
         print("None")
         return
 
-    user_agent = {'User-agent': 'MyBot/1.0'}
+    user_agent = {'User-agent': 'Google Chrome Version 24.0.6367.37'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
     try:
-        response = get(url, headers=user_agent, params=params)
-        response.raise_for_status()  # Raise an exception for any HTTP errors
-        results = response.json()
         my_data = results.get('data').get('children')
 
-        for post in my_data:
-            print(post.get('data').get('title'))
+        for i in my_data:
+            print(i.get('data').get('title'))
 
-    except Exception as e:
-        print("Error:", e)
+    except Exception:
+        print("None")
