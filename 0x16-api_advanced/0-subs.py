1f0013c63bcd529f9363f44461850bd2562f08e7
Aellun <okeelo.k.otieno@gmail.com>
2024-04-11 20:11:06 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit. return 0"""
+import request
 
 
 def number_of_subscribers(subreddit):
